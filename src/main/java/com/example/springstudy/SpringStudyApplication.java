85cba34299bb697e4a659dc9b44cd79de711028c
이수정 <developercosj@gmail.com>
2023-11-25 23:43:42 +0900

    [이수정] 간단한 회원 관리 예제 생성
    - view html 파일 생성
    - controller의 이름이 동일한 GetMapping과 PostMapping 나눠서 사용
    - 파라미터로 전달하는 객체 생성
    
    *인프런 (김영한) 스프링 입문

--- src/main/java/com/example/springstudy/SpringStudyApplication.java
+++ src/main/java/com/example/springstudy/SpringStudyApplication.java
@@ -2,8 +2,9 @@ package com.example.springstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SpringStudyApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/springstudy/controller/MemberController.java
+++ src/main/java/com/example/springstudy/controller/MemberController.java
@@ -4,6 +4,11 @@ import com.example.springstudy.domain.Member;
 import com.example.springstudy.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -29,6 +34,30 @@ public class MemberController {
     //     public void setMemberService(MemberService memberService) {
     //        this.memberService = memberService;
     //     }
+
+    // 조회할때 주로 @GetMapping
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    // 데이터 전달 (form) 주로 @PostMapping
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 
 
 

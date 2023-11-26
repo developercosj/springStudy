850f53f73a41adfc977c9798db8b080bba6ea1a4
이수정 <developercosj@gmail.com>
2023-11-26 23:50:28 +0900

    [이수정] h2 DB 설정 및 순수 JDBC 코드 작성 연습
    interface 확장

--- src/main/java/com/example/springstudy/SpringConfig.java
+++ src/main/java/com/example/springstudy/SpringConfig.java
@@ -1,14 +1,26 @@
 package com.example.springstudy;
 
+import com.example.springstudy.repository.JdbcMemberRepository;
 import com.example.springstudy.repository.MemberRepository;
 import com.example.springstudy.repository.MemoryMemberRepository;
 import com.example.springstudy.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    //@Autowired  DataSource dataSource;
+    // 생성자 주입으로 변경
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     // 스프링이 올라올때 memberService와 memberRepository를 스프링 빈에 등록해준다.
     // Controller는 @Bean으로 직접 등록하는 대신에 스프링이 제공하는 컴포넌트 스캔이라는 기능을 통해 자동으로 스프링 빈으로 등록
@@ -19,7 +31,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
---
+++ src/main/java/com/example/springstudy/repository/JdbcMemberRepository.java
@@ -0,0 +1,109 @@
+package com.example.springstudy.repository;
+
+import com.example.springstudy.domain.Member;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcMemberRepository implements MemberRepository {
+
+
+    private final DataSource dataSource;
+
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(name) values(?)";
+        Connection conn = null;
+
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            // id 값 리턴
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            // parameterIndex 는 ? 부분
+            pstmt.setString(1, member.getName());
+            // DB 에 쿼리 날림
+            pstmt.executeUpdate();
+            // Statement.RETURN_GENERATED_KEYS 과 매칭 key 의 index 반환해줌
+            rs = pstmt.getGeneratedKeys();
+            // 값이 있으면 값을 꺼내서 세팅
+            if (rs.next()) {
+                member.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            };
+            return member;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+
+
+    }
+
+    private Connection getConnection() {
+        // DataSourceUtils를 통해 connection 획득해야 transaction 깨끗하게 유지가능
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+    // try-with-resources 로도 작성 가능 (알아보기)
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+       // 역순으로 close
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        }  catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            if (conn != null) {
+                close(conn);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+
+    // 나머지 코드는 생략함
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}

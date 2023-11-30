203759f1242431bf0570481471ac12a4f4b707d4
이수정 <developercosj@gmail.com>
2023-11-30 14:56:01 +0900

    [이수정] JPA, JPQL 사용하여 회원 서비스 및 테스트 코드 작성

--- src/main/java/com/example/springstudy/SpringConfig.java
+++ src/main/java/com/example/springstudy/SpringConfig.java
@@ -1,10 +1,8 @@
 package com.example.springstudy;
 
-import com.example.springstudy.repository.JdbcMemberRepository;
-import com.example.springstudy.repository.JdbcTemplateMemberRepository;
-import com.example.springstudy.repository.MemberRepository;
-import com.example.springstudy.repository.MemoryMemberRepository;
+import com.example.springstudy.repository.*;
 import com.example.springstudy.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,13 +14,18 @@ public class SpringConfig {
 
     //@Autowired  DataSource dataSource;
     // 생성자 주입으로 변경
-    private DataSource dataSource;
+    // private DataSource dataSource;
+    //@Autowired
+    //public SpringConfig(DataSource dataSource) {
+    //    this.dataSource = dataSource;
+    //}
 
+    // 생성자 주입 (@PersitenceContext 로도 가능)
+    private EntityManager em;
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
-
     // 스프링이 올라올때 memberService와 memberRepository를 스프링 빈에 등록해준다.
     // Controller는 @Bean으로 직접 등록하는 대신에 스프링이 제공하는 컴포넌트 스캔이라는 기능을 통해 자동으로 스프링 빈으로 등록
     @Bean
@@ -34,7 +37,8 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
--- src/main/java/com/example/springstudy/domain/Member.java
+++ src/main/java/com/example/springstudy/domain/Member.java
@@ -1,12 +1,16 @@
 package com.example.springstudy.domain;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
 @Getter
+@Entity
 public class Member {
 
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
